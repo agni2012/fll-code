e106f19827cd84e51725a76c2c4b526b12163e27
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2025-09-25 20:10:56 -0700

    Update and rename Run1.py to Run2.py
    ;,.pyfgcrl qwertyu sucks

--- Run2.py
+++ Run2.py
@@ -28,17 +28,19 @@ while True:
     if(arm_motor.load() > 30):
         break;
 
-arm_motor.run_angle(100, 30)
+arm_motor.run_angle(200, 30)
 
 robot.use_gyro(True)
 robot.straight(20)
 robot.turn(90)
-robot.straight(520-20)
-robot.straight(-105)
+robot.straight(520-30)
+robot.straight(-95)
 robot.turn(-44)
 robot.straight(385)
-arm_motor.run_angle(100, 160)
+arm_motor.run_angle(200, 160)
 robot.turn(-17)
-robot.straight(57)
-arm_motor.run_angle(100, -150, wait = True);
-robot.straight(50);
+robot.straight(64)
+arm_motor.run_angle(200, -150, wait = True)
+robot.turn(-45)
+#todo: get scale pan and get to home 2 (unearthed one)
+
