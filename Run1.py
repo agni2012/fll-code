7beec1c87987c5fb7d4ed32603e620c6a2d2129c
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2025-09-26 19:50:36 -0700

    Update Run1.py

--- Run1.py
+++ Run1.py
@@ -17,11 +17,10 @@ arm_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 robot = DriveBase(l_motor, r_motor, 56, 159)
 
 robot.settings(
-    straight_speed = 180,
-    straight_acceleration = 275,
-    turn_rate = 150
+    straight_speed = 350,
+    straight_acceleration = 200,
+    turn_rate = 200
 )
-#"""y
 
 arm_motor.run_angle(200, 30)
 
@@ -35,20 +34,39 @@ while True:
 arm_motor.run_angle(200, 30)
 
 robot.use_gyro(True)
-arm_motor.run_angle(100,102.5)
+arm_motor.run_angle(100,103.14159265358979323)
 robot.straight(530)
 robot.straight(-100)
 robot.turn(10)
 wait(500)
 robot.straight(30)
 robot.turn(-15)
-arm_motor.run_angle(100,-50)
+arm_motor.run_angle(50,-50)
 robot.straight(-250)
-arm_motor.run_angle(100,-50)
+arm_motor.run_angle(700,-50)
 robot.turn(20)
-robot.straight(580)
-robot.turn(60)
+robot.straight(490)
+robot.turn(55)
 robot.straight(50)
-arm_motor.run_angle()
+arm_motor.run_angle(100,90)
 robot.turn(20)
-robot.straight(200)
+arm_motor.run_angle(100,30)
+robot.straight(70)
+arm_motor.run_angle(50,-130, wait = False);
+robot.drive(30, 0)
+wait(4000)
+arm_motor.stop()
+
+robot.straight(100)
+
+arm_motor.run_angle(50, 60);
+robot.turn(-30)
+robot.straight(170)
+robot.turn(50)
+
+robot.straight(120)
+robot.turn(50)
+robot.straight(167)
+robot.turn(50)
+robot.straight(-160)
+arm_motor.run_angle(100, -60)
