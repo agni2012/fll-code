7d5efb67bd5b1df11630a6cbd8534c07aab31040
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2025-10-05 08:29:26 -0700

    Update Run1.py

--- Run1.py
+++ Run1.py
@@ -1,3 +1,7 @@
+# PortA is right arm motor
+# PortB is left arm motor
+# PortD is left drive motor
+# PortF is right drive motor
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
@@ -13,60 +17,52 @@ print("\x1b[H\x1b[2J", end="")
 print('Battery: ' + str(round((battery-6000)/23)) + '%')
 l_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
 r_motor = Motor(Port.F)
-arm_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+l_arm_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+r_arm_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 robot = DriveBase(l_motor, r_motor, 56, 159)
 
 robot.settings(
-    straight_speed = 350,
-    straight_acceleration = 200,
+    straight_speed = 400,
+    straight_acceleration = 300,
     turn_rate = 200
 )
-
-arm_motor.run_angle(200, 30)
-
-arm_motor.run_angle(200, -15)
-
-while True:
-    arm_motor.run(-100)
-    if(arm_motor.load() > 50):
-        break;
-
-arm_motor.run_angle(200, 30)
-
 robot.use_gyro(True)
-arm_motor.run_angle(100,103.14159265358979323)
-robot.straight(530)
-robot.straight(-100)
+l_arm_motor.run_target(100,170)
+robot.straight(540)
+robot.straight(-110)
 robot.turn(10)
 wait(500)
 robot.straight(30)
 robot.turn(-15)
-arm_motor.run_angle(50,-50)
+l_arm_motor.run_angle(50,-50)
 robot.straight(-250)
-arm_motor.run_angle(700,-50)
+l_arm_motor.run_angle(700,-50)
 robot.turn(20)
 robot.straight(490)
 robot.turn(55)
 robot.straight(50)
-arm_motor.run_angle(100,90)
+l_arm_motor.run_angle(100,90)
 robot.turn(20)
-arm_motor.run_angle(100,30)
 robot.straight(70)
-arm_motor.run_angle(50,-130, wait = False);
+l_arm_motor.run_angle(50,-130, wait = False);
 robot.drive(30, 0)
 wait(4000)
-arm_motor.stop()
+l_arm_motor.stop()
 
 robot.straight(100)
 
-arm_motor.run_angle(50, 60);
+l_arm_motor.run_angle(50, 60);
 robot.turn(-30)
-robot.straight(170)
-robot.turn(50)
+robot.straight(200)
+robot.turn(45)
+robot.straight(280)
+robot.turn(22)
+robot.settings(straight_speed=100)
+robot.straight(150)
+robot.settings(straight_speed=400)
+robot.straight(-200)
 
-robot.straight(120)
-robot.turn(50)
-robot.straight(167)
-robot.turn(50)
-robot.straight(-160)
-arm_motor.run_angle(100, -60)
+robot.turn(30)
+robot.straight(200)
+robot.turn(10)
+robot.straight(-200)
