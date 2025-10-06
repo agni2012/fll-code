ca8c50eaaa75bfbdcbf86f2372fc4702a8c8e96e
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2025-10-05 19:30:41 -0700

    Create anirvs_reeveel_of_de_map.py

---
+++ anirvs_reeveel_of_de_map.py
@@ -0,0 +1,39 @@
+# PortA is right arm motor
+# PortB is left arm motor
+# PortD is left drive motor
+# PortF is right drive motor
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+total = 0
+for i in range(100):
+    total += hub.battery.voltage()
+battery = total / 100
+print("\x1b[H\x1b[2J", end="")
+print('Battery: ' + str(round((battery-6000)/23)) + '%')
+l_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+r_motor = Motor(Port.F)
+l_arm_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+r_arm_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+robot = DriveBase(l_motor, r_motor, 56, 159)
+
+robot.settings(
+    straight_speed = 500,
+    straight_acceleration = 500,
+    turn_rate = 150
+)
+robot.use_gyro(True)
+r_arm_motor.run_angle(100, 155)
+robot.straight(600)
+robot.turn(-36)
+robot.straight(175)
+r_arm_motor.run_angle(100,-150)
+#robot.straight(30)
+robot.straight(-200)
+robot.turn(36)
+robot.straight(-605)
+robot.turn(-50)
