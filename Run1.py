e99576853aeb506dc61de38161b456fb7aa25bd4
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2025-10-18 14:28:04 -0700

    Refactor robot movement and arm motor settings
    Updated robot settings and adjusted arm motor angles for various movements. Modified the sequence for the brush mission and mineshaft exploration.

--- Run1.py
+++ Run1.py
@@ -15,10 +15,9 @@ l_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
 r_motor = Motor(Port.F)
 arm_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 robot = DriveBase(l_motor, r_motor, 56, 159)
-
 robot.settings(
-    straight_speed = 350,
-    straight_acceleration = 200,
+    straight_speed = 500,
+    straight_acceleration = 700,
     turn_rate = 200
 )
 
@@ -34,36 +33,36 @@ while True:
 arm_motor.run_angle(200, 30)
 
 robot.use_gyro(True)
-arm_motor.run_angle(100,100.14159265358979323)
+arm_motor.run_angle(100,105.14159265358979323846264338327950288)
 
 #brush mission
 robot.straight(530)
-robot.straight(-100)
-robot.turn(10)
-wait(500)
-robot.straight(30)
-robot.turn(-15)
-arm_motor.run_angle(50,-50)
-robot.straight(-250)
-arm_motor.run_angle(70,-50)
 
-#Aayana (mineshaft explorer)
-robot.turn(20)
-robot.straight(490)
-robot.turn(55)
-robot.straight(50)
-arm_motor.run_angle(100,90)
-robot.turn(20)
-arm_motor.run_angle(100,35)
-robot.straight(75)
-arm_motor.run_angle(50,-130, wait = False);
-robot.drive(29, 0)
-wait(4000)
+robot.straight(-55, then = Stop.NONE)
+arm_motor.run_angle(200,-99, wait = False)
+
+robot.straight(-295)
+
+arm_motor.run_angle(50,110, wait = False)
+robot.straight(300, then =  Stop.NONE)
+robot.settings(
+    straight_speed = 200
+)
+
+robot.arc(195, angle = 90, then = Stop.NONE)
+robot.settings(
+    straight_speed = 500
+)
+
+#Mineshaft explorer)
+robot.straight(157)
+arm_motor.run_angle(270,-75, wait = False);
+robot.straight(100, then= Stop.NONE)
 arm_motor.stop()
 
 #whats on the scale
 robot.straight(105)
-arm_motor.run_angle(50, 60, wait=False);
+arm_motor.run_angle(50, 70, wait=False);
 robot.turn(-30)
 robot.straight(200)
 robot.turn(45)
@@ -74,10 +73,15 @@ robot.straight(150)
 robot.settings(straight_speed=400)
 robot.straight(-200)
 
-#tip the scale
-robot.turn(90)
-robot.straight(60) #was 150
-robot.turn(-30)
-robot.straight(-80) #was 60
-robot.turn(-20)
-arm_motor.run_angle(300,-90)
+robot.turn(10)
+
+robot.turn(50)
+arm_motor.run_angle(100, -50);
+
+robot.straight(150)
+arm_motor.run_angle(200, -100,wait=False)
+wait(500)
+arm_motor.run_angle(100, 100, wait = False)
+robot.settings(straight_acceleration=1000)
+robot.arc(400, angle=90, then = Stop.NONE)
+robot.arc(-1000, distance=600)
